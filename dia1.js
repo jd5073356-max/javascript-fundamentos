a3c54df6938472fcee0ef25fbe053d44d13f9354
jd5073356-max <jd5073356@gmail.com>
2026-03-11 20:36:40 -0500

    Create dia1.js
    primer dia  aprendiendo javascript y en si programar

---
+++ dia1.js
@@ -0,0 +1,15 @@
+//dia 1 -variables en javascript
+
+let nombre="jaun"
+let edad=17
+let ciudad="bogota"
+let profesion="desarrollador"
+let precio=180
+let descuento=20
+let total=precio-descuento 
+
+console.log(nombre)
+console.log(edad)
+console.log(ciudad)
+console.log(profesion)
+console.log(total)
