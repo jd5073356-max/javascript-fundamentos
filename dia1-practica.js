b25683a0d7f3f2db06b04190637ebd66833bcce4
jd5073356-max <jd5073356@gmail.com>
2026-03-11 21:26:01 -0500

    Create dia1-practica.js

---
+++ dia1-practica.js
@@ -0,0 +1,40 @@
+//ejercisio1 let y  const
+
+let nombre="juan"
+let edad=17
+const pais="colombia"//const es constante el dato siempre se mantiene igual a cambio de let que ahi si varia el contenido de su casilla
+
+console.log(nombre)
+console.log(edad)
+console.log(pais)
+
+//ahora cambia el nombre
+nombre="juan david"
+console.log(nombre)
+
+//ejercisio 2 - tipos de datos
+let texto="hola soy juan"
+let numero=25
+let decimal=1.75
+let verdadero=true
+let falso=false
+
+ console.log(texto)
+ console.log(numero)
+ console.log(decimal)
+ console.log(verdadero)
+ console.log(falso)
+
+ //diferencia entre numero con y sin comillas
+ let edadnumero=17
+ let edadtexto="17"
+
+ console.log(edadnumero +3)  //suma
+ console.log(edadtexto +3)  //¿que crees que pasa aqui? se pone el texto osea 17 y al lado el numero que seria 3 haci dando 173
+
+ let tunombre="juan"
+ let tuedad=17 
+let mayorde_edad=false
+console.log(tunombre)
+ console.log(tuedad)
+console.log(mayorde_edad)
